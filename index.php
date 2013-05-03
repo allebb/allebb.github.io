d199f58a2314fea9b9876d4dda04e4c010a73e17
Bobby Allen <bobbyallen.uk@gmail.com>
2013-05-03 23:43:22 +0100

    Added dynamic favicon using my Gravatar image.

--- index.php
+++ index.php
@@ -40,11 +40,11 @@ $avatar_size = 220;
         <![endif]-->
 
         <!-- Fav and touch icons -->
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
-        <link rel="shortcut icon" href="ico/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://www.gravatar.com/avatar/<?php echo $avatar_url; ?>?s=144">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://www.gravatar.com/avatar/<?php echo $avatar_url; ?>?s=144">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://www.gravatar.com/avatar/<?php echo $avatar_url; ?>?s=72">
+        <link rel="apple-touch-icon-precomposed" href="http://www.gravatar.com/avatar/<?php echo $avatar_url; ?>">
+        <link rel="shortcut icon" href="http://www.gravatar.com/avatar/<?php echo $avatar_url; ?>?s=16">
     </head>
 
     <body>
