65edeac02576e2b5dde8f7b87ff94e78fcd971c1
Bobby Allen <bobbyallen.uk@gmail.com>
2013-07-15 21:08:51 +0100

    Added author tag content (must have missed it off before.) also added keywords too.

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ $avatar_size = 220;
         <title>Bobby Allen (ballen/bobsta63)</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="I'm Bobby Allen, a software developer from Ipswich, England.">
-        <meta name="author" content="">
+        <meta name="keywords" content="Bobby Allen, ballen, bobsta63, Ipswich, software, web, developer">
+        <meta name="author" content="Bobby Allen">
         <link href="css/bootstrap.css" rel="stylesheet">
         <style type="text/css">
             body {
@@ -33,12 +34,10 @@ $avatar_size = 220;
 
         </style>
         <link href="css/bootstrap.css" rel="stylesheet">
-
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
           <script src="../assets/js/html5shiv.js"></script>
         <![endif]-->
-
         <!-- Fav and touch icons -->
         <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://www.gravatar.com/avatar/<?php echo $avatar_url; ?>?s=144">
         <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://www.gravatar.com/avatar/<?php echo $avatar_url; ?>?s=144">
