2eb9664ab74cc96c36c4e1623d11c2ef37a57d44
Bobby Allen <ballen@bobbyallen.me>
2014-01-25 18:54:41 +0000

    Updated tracking code to match my new Google Analytics account.
    Updated tracking code to match my new Google Analytics account.

--- index.php
+++ index.php
@@ -109,21 +109,14 @@ $avatar_size = 220;
                 selector: "a[data-toggle=tooltip]"
             });
         </script>
-        <script type="text/javascript">
-
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-39380946-1']);
-            _gaq.push(['_trackPageview']);
-
-            (function() {
-                var ga = document.createElement('script');
-                ga.type = 'text/javascript';
-                ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0];
-                s.parentNode.insertBefore(ga, s);
-            })();
-
+        <script>
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            
+              ga('create', 'UA-47482933-1', 'bobbyallen.me');
+              ga('send', 'pageview');
         </script>
     </body>
 </html>
